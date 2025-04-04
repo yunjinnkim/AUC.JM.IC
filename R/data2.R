0139f9b93a488d2fffa31fa5754829cca413a7d0
yunjinnkim <dbswls629@sookmyung.ac.kr>
2025-04-05 00:17:46 +0900

    Update README.md

---
+++ R/data2.R
@@ -0,0 +1,21 @@
+#' Example dataset: data2
+#'
+#' Description of data2.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{BVRT}{Brief Visuospatial Memory Test score}
+#'   \item{CEP}{Cognitive Examination Performance score}
+#'   \item{CESD}{Center for Epidemiologic Studies Depression scale score}
+#'   \item{HIER}{Hierarchical score}
+#'   \item{ID}{Unique subject identifier}
+#'   \item{IST}{Intelligence Structure Test score}
+#'   \item{MMSE}{Mini-Mental State Examination score}
+#'   \item{age}{Current age of the subject}
+#'   \item{age_init}{Age at initial assessment}
+#'   \item{agedem}{Age at dementia onset (if applicable)}
+#'   \item{dem}{Dementia diagnosis (1 = Yes, 0 = No)}
+#'   \item{male}{Gender indicator (1 = Male, 0 = Female)}
+#' }
+#' @source Source of data
+"data2"
---
+++ R/data2id.R
@@ -0,0 +1,7 @@
+#' Example dataset: data2id
+#'
+#' Description of data2id.
+#'
+#' @format A vector with length X.
+#' @source Source of data
+"data2id"
